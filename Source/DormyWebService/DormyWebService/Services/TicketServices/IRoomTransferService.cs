bd8ff2f237f9f5fd85cf8301ed5e69d5811d62d0
DATNT <tandat1405@gmail.com>
2019-10-28 19:35:42 +0700

    resolve transfer

--- Source/DormyWebService/DormyWebService/Services/TicketServices/IRoomTransferService.cs
+++ Source/DormyWebService/DormyWebService/Services/TicketServices/IRoomTransferService.cs
@@ -1,10 +1,20 @@
-﻿using DormyWebService.ViewModels.TicketViewModels.RoomTransfer.SendRoomTransfer;
+﻿using DormyWebService.Entities.TicketEntities;
+using DormyWebService.ViewModels.TicketViewModels.RoomBooking.ResolveRoomBooking;
+using DormyWebService.ViewModels.TicketViewModels.RoomTransfer.SendRoomTransfer;
+using DormyWebService.ViewModels.TicketViewModels.RoomTransferRequestViewModels.ResolveRoomTransferRequest;
 using System.Threading.Tasks;
 
 namespace DormyWebService.Services.TicketServices
 {
     public interface IRoomTransferService
     {
+        Task<RoomTransferRequestForm> FindById(int id);
         Task<SendRoomTransferRespone> SendRequest(SendRoomTransferRequest request);
+        Task<bool> DeleteRoomTransfer(int id);
+        //Task<bool> EditRoomRequest(EditRoomTransferRequest request);
+
+        Task<ResolveRoomTransferResponse> ResolveRequest(ResolveRoomTransferRequest request);
+
+
     }
 }
--- Source/DormyWebService/DormyWebService/Services/TicketServices/RoomTransferService.cs
+++ Source/DormyWebService/DormyWebService/Services/TicketServices/RoomTransferService.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using AutoMapper;
+using DormyWebService.Entities.AccountEntities;
+using DormyWebService.Entities.ContractEntities;
 using DormyWebService.Entities.TicketEntities;
 using DormyWebService.Repositories;
 using DormyWebService.Services.ParamServices;
 using DormyWebService.Services.UserServices;
 using DormyWebService.Utilities;
+using DormyWebService.ViewModels.TicketViewModels.RoomBooking.ResolveRoomBooking;
 using DormyWebService.ViewModels.TicketViewModels.RoomTransfer.SendRoomTransfer;
+using DormyWebService.ViewModels.TicketViewModels.RoomTransferRequestViewModels.ResolveRoomTransferRequest;
 using Sieve.Services;
 
 namespace DormyWebService.Services.TicketServices
@@ -31,6 +36,18 @@ namespace DormyWebService.Services.TicketServices
             _userService = userService;
         }
 
+        public async Task<RoomTransferRequestForm> FindById(int id)
+        {
+            var result = await _repoWrapper.RoomTransfer.FindByIdAsync(id);
+
+            if (result == null)
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.NotFound, "RoomTransferService: Room Transfer is not found");
+            }
+
+            return result;
+        }
+
         public async Task<SendRoomTransferRespone> SendRequest(SendRoomTransferRequest request)
         {
             //Check if room exist
@@ -45,12 +62,11 @@ namespace DormyWebService.Services.TicketServices
             await _studentService.FindById(request.StudentId);
 
             //Check if there's active room transfer request
-            //Chỗ này ko phải _repoWrapper.RoomBooking mà là _repoWrapper.RoomTransfer
-            var bookings = (List<RoomTransferRequestForm>)
+            var transfers = (List<RoomTransferRequestForm>)
                 await _repoWrapper.RoomTransfer.FindAllAsyncWithCondition(r => r.StudentId == request.StudentId);
-            if (bookings!= null)
+            if (transfers != null)
             {
-                if (bookings.Exists(b => b.Status == RequestStatus.Pending))
+                if (transfers.Exists(b => b.Status == RequestStatus.Pending))
                 {
                     throw new HttpStatusCodeException(HttpStatusCode.Forbidden, "RoomTransferService: There are already active transfer requests for this account");
                 }
@@ -58,7 +74,7 @@ namespace DormyWebService.Services.TicketServices
 
             //TODO: check if contract is valid
 
-            //Create new room booking from request
+            //Create new room reansfer from request
             var result = SendRoomTransferRequest.NewEntityFromRequest(request);
 
             //Create in database
@@ -67,6 +83,62 @@ namespace DormyWebService.Services.TicketServices
             {
                 RoomTransferFormId = result.RoomTransferRequestFormId
             };
+        }
+
+        public async Task<ResolveRoomTransferResponse> ResolveRequest(ResolveRoomTransferRequest request)
+        {
+            //Check if this staff exist
+            var staff = await _repoWrapper.Staff.FindByIdAsync(request.StaffId);
+
+            if (staff == null)
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.BadRequest, "RoomTransferService: Provide StaffId is not a staff");
+            }
+
+            //Check if Room Transfer Request Exists
+            var roomTransfer = await FindById(request.RoomTransferRequestFormId);
+
+            if (roomTransfer.Status == request.Status)
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.BadRequest, "RoomTransferService: This request already has this status");
+            }
+
+            if (roomTransfer.Status == RequestStatus.Complete)
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.BadRequest, "RoomTransferService: This request is already completed, can't change it to anything else''");
+            }
+
+            //Update information into DataSet
+            roomTransfer = _mapper.Map(request, roomTransfer);
+
+            //Update Last Updated Date
+            roomTransfer.LastUpdated = DateTime.Now;
+
+            //Update to database
+            roomTransfer =
+                    await _repoWrapper.RoomTransfer.UpdateAsyncWithoutSave(roomTransfer, roomTransfer.RoomTransferRequestFormId);
+
+            //If request is changed to complete
+            if (request.Status == RequestStatus.Complete)
+            {
+                //Get contract that have the same studentId
+                System.Diagnostics.Debug.WriteLine("roomTransfer.StudentId: " + roomTransfer.StudentId);
+                var tempStudent = await
+                    _repoWrapper.Student.FindAllAsyncWithCondition(c => c.StudentId == roomTransfer.StudentId);
+                //TODO change student's room
+            }
+
+            //Save to database at once
+            //await _repoWrapper.Save();
+
+            //Return mapped response
+            //return _mapper.Map<ResolveRoomBookingResponse>(roomTransfer);
+            return null;
+        }
+
+        public async Task<bool> DeleteRoomTransfer(int id)
+        {
+            return await _repoWrapper.RoomTransfer.DeleteAsync(await FindById(id)) > 0;
         }
     }
 }
