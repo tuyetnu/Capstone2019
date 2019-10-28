f6838d337ab680c06a6b4036c74462cdbe8dd3f1
ComradeWang <tphan2883@gmail.com>
2019-10-28 18:08:22 +0700

    Before merge

---
+++ Source/DormyWebService/DormyWebService/ViewModels/RoomViewModels/ArrangeRoom/ArrangeRoomResponseStudent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DormyWebService.Entities.AccountEntities;
+using DormyWebService.Entities.RoomEntities;
+
+namespace DormyWebService.ViewModels.RoomViewModels.ArrangeRoom
+{
+    public class ArrangeRoomResponseStudent
+    {
+        public int StudentId { get; set; }
+        public int RoomId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string RoomName { get; set; }
+    }
+}
