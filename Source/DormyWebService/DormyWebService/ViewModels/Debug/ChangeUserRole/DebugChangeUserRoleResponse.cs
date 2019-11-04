efa7588c70668bc2d6970408fd6d7caed2ab5266
Hai Nguyen <nunttse62525@fpt.edu.vn>
2019-11-04 22:14:08 +0700

    miss file

---
+++ Source/DormyWebService/DormyWebService/ViewModels/Debug/ChangeUserRole/DebugChangeUserRoleResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DormyWebService.ViewModels.Debug.ChangeUserRole
+{
+    public class DebugChangeUserRoleResponse
+    {
+        public int UserId { get; set; }
+        public string Role { get; set; }
+    }
+}
