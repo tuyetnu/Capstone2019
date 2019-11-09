a2d1b533e0675fffd6ef5bd628409db1768a9d43
DATNT <tandat1405@gmail.com>
2019-11-09 16:20:30 +0700

    status

---
+++ Source/DormyWebService/DormyWebService/Migrations/20191109091243_4.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DormyWebService.Migrations
+{
+    public partial class _4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "CancelContractForms",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "CancelContractForms",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
