20e4519104007d61c69682af16525f297d1817f3
DATNT <tandat1405@gmail.com>
2019-11-09 16:21:01 +0700

    on doing

--- Source/DormyWebService/DormyWebService/Services/TicketServices/CancelContractService.cs
+++ Source/DormyWebService/DormyWebService/Services/TicketServices/CancelContractService.cs
@@ -1,7 +1,49 @@
-﻿namespace DormyWebService.Services.TicketServices
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using DormyWebService.Entities.ContractEntities;
+using DormyWebService.Repositories;
+using DormyWebService.Services.ParamServices;
+using DormyWebService.Services.UserServices;
+using DormyWebService.Utilities;
+using DormyWebService.ViewModels.TicketViewModels.CancelContract.SendCancelContractRequest;
+using Microsoft.AspNetCore.Mvc;
+using Sieve.Services;
+
+namespace DormyWebService.Services.TicketServices
 {
     public class CancelContractService : ICancelContractService
     {
-        
+        private readonly IRepositoryWrapper _repoWrapper;
+        private readonly IStudentService _studentService;
+        private readonly ISieveProcessor _sieveProcessor;
+        private readonly IParamService _paramService;
+        private readonly IStaffService _staffService;
+
+        public CancelContractService(IRepositoryWrapper repoWrapper, IStudentService studentService, ISieveProcessor sieveProcessor, IParamService paramService, IStaffService staffService)
+        {
+            _repoWrapper = repoWrapper;
+            _studentService = studentService;
+            _sieveProcessor = sieveProcessor;
+            _paramService = paramService;
+            _staffService = staffService;
+        }
+        public async Task<ActionResult<SendCancelContractFormResponse>> SendRenewContract(SendCancelContractFormRequest request)
+        {
+            throw new System.NotImplementedException();
+            var student = await _studentService.FindById(request.StudentId);
+            if (student.RoomId == null)
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.Forbidden, "CancelContractService: Student doesn't have room");
+            }
+            var contracts = (List<Contract>)
+                await _repoWrapper.Contract.FindAllAsyncWithCondition(c => c.StudentId == student.StudentId && c.Status == ContractStatus.Active);
+            if (contracts == null || !contracts.Any())
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.NotFound, "RenewContractService: Student doesn't have any active contract'");
+            }
+            //var cancelContract = SendCancelContractFormRequest
+        }
     }
 }
---
+++ Source/DormyWebService/DormyWebService/ViewModels/TicketViewModels/CancelContract/SendCancelContractRequest/SendCancelContractFormRequest.cs
@@ -0,0 +1,35 @@
+﻿using DormyWebService.Entities.ContractEntities;
+using DormyWebService.Entities.TicketEntities;
+using DormyWebService.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DormyWebService.ViewModels.TicketViewModels.CancelContract.SendCancelContractRequest
+{
+    public class SendCancelContractFormRequest
+    {
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(500)]
+        public string Reason { get; set; }
+
+        public static CancelContractForm EntityFromRequest(SendCancelContractFormRequest request, Contract contract)
+        {
+            return new CancelContractForm()
+            {
+                //Month = request.Month,
+                StudentId = request.StudentId,
+                //ContractId = contract.ContractId,
+                CreatedDate = DateTime.Now.AddHours(GlobalParams.TimeZone),
+                LastUpdated = DateTime.Now.AddHours(GlobalParams.TimeZone),
+                Status = RequestStatus.Pending,
+
+            };
+        }
+    }
+}
